08d729a2c8ca5beae2a625b4235ba1dd37e19fb8
Jaasiel <69393570+JaasielAntunes@users.noreply.github.com>
2023-05-22 17:25:30 -0300

    Screenshots

--- src/components/NewMemoryForm.tsx
+++ src/components/NewMemoryForm.tsx
@@ -9,6 +9,7 @@ import { useRouter } from 'next/navigation'
 
 export function NewMemoryForm() {
   const router = useRouter()
+  const refresh = () => window.location.reload()
 
   async function handleCreateMemory(event: FormEvent<HTMLFormElement>) {
     event.preventDefault()
@@ -79,14 +80,15 @@ export function NewMemoryForm() {
         name="content"
         spellCheck={false}
         className="w-full flex-1 resize-none rounded border-0 bg-transparent p-0 text-lg leading-relaxed text-gray-100 placeholder:text-gray-400 focus:ring-0"
-        placeholder="Fique livre para adicionar fotos, vídeos e relatos sobre essa experiência que você quer lembrar para sempre."
+        placeholder="Fique livre para adicionar fotos, vídeos e relatos sobre uma experiência que você quer lembrar para sempre."
       />
 
       <button
         type="submit"
         className="inline-block self-end rounded-full bg-green-500 px-5 py-3 font-alt text-sm uppercase leading-none text-black hover:bg-green-600"
+        onClick={refresh}
       >
-        Salvar Memória
+        salvar memória
       </button>
     </form>
   )
